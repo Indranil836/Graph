c111888727ddaf3f305d5b3468edf313dc702f32
Indranil Roy <indranilroy836@gmail.com>
2022-10-12 12:39:23 +0530

    first commit

---
+++ bfs.java
@@ -0,0 +1,22 @@
+public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    // Code here
+    ArrayList<Integer> bfs = new ArrayList<>();
+    Queue<Integer> q = new LinkedList<>();
+    boolean visited[]=new boolean[V];
+    q.add(0);
+    visited[0]=true;
+    while(!q.isEmpty()){
+        int node = q.poll();
+        bfs.add(node);
+        
+        for(Integer it:adj.get(node)){
+            if(visited[it]==false){
+                visited[it]=true;
+                q.add(it);
+                
+            }
+        }
+    }
+    return bfs;
+    
+}
